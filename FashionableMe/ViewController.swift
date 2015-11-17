3c06f52b01b4d714ec8c1a68155e050bfcd8679c
Aditya Yadav <aditya@macbook-pro.local>
2015-11-17 18:38:15 +0530

    Initial Commit

---
+++ FashionableMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FashionableMe
+//
+//  Created by adi on 17/11/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
